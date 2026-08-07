e56826d9f98c54ab6cb1710076af658fc55836fb
pinegam2847 <pinegam2847@gmail.com>
2026-08-07 15:38:09 +0900

    08071538

--- 0850_RE/script.js
+++ 0850_RE/script.js
@@ -1,1 +1,8 @@
-// test
+const modal = document.querySelector('#modal');
+document.querySelector('#contactBtn').addEventListener('click', () => {
+    modal.classList.add('is-open');
+});
+
+document.querySelector('#closeBtn').addEventListener('click', () => {
+    modal.classList.remove('is-open');
+});
